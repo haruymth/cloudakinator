2af67ad96af4e6b5fbbaee2b7455e42bd0318629
haruymth <108157117+haruymth@users.noreply.github.com>
2023-03-08 15:17:46 +0900

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,7 +1,10 @@
+const fetch = require("@replit/node-fetch");
 const log = (d)=>{
+  console.log(d);
   try{
     fetch("https://Logger.haru-ymth.repl.co",{method:"POST",body:d});
   }catch(error){
+    console.log(error);
     return;
   }
 }
